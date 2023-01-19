a37f04104993a3207cede0e8b7eb3a9db5832260
skvsbk <skv.sbk@mail.ru>
2023-01-19 10:01:29 +0300

    It is not need

--- app/routers/depends.py
+++
@@ -1,8 +0,0 @@
-from app.utils.users import UserUtils, RoleUtils
-from app.models.database import database
-
-def get_user_utils() -> UserUtils:
-    return UserUtils(database)
-
-def get_role_utils() -> RoleUtils:
-    return RoleUtils(database)
