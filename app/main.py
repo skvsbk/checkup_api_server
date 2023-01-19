9610e2003b7185871b9a2cbf5b8e66849099ea17
skvsbk <skv.sbk@mail.ru>
2023-01-19 10:01:41 +0300

    No changes

--- app/main.py
+++ app/main.py
@@ -3,14 +3,17 @@ from app.routers import checkups, facilities, nfc, valparams, routes, users
 import uvicorn
 from app.models.database import database
 
+from app.models.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Electronic journal of inspections')
 app.include_router(checkups.router, prefix='/checkups', tags=['checkups'])
 app.include_router(facilities.router, prefix='/facilities', tags=['facilities'])
 app.include_router(nfc.router, prefix='/nfc', tags=['nfc'])
-app.include_router(params.router, prefix='/params', tags=['params'])
+app.include_router(valparams.router, prefix='/params', tags=['params'])
 app.include_router(routes.router, prefix='/routes', tags=['routes'])
-app.include_router(users.router) # , prefix='/users', tags=['users'])
+app.include_router(users.router, prefix='/users', tags=['users'])
 
 
 @app.on_event('startup')
