3aaff83a63ec8d6c90b5c735d1d09620373b44a7
skvsbk <skv.sbk@mail.ru>
2023-01-19 16:38:38 +0300

    refactoring

--- app/models/database.py
+++ app/models/database.py
@@ -1,8 +1,5 @@
-from databases import Database
 from sqlalchemy import create_engine, MetaData
 from starlette.config import Config
-
-#add
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -10,12 +7,8 @@ from sqlalchemy.orm import sessionmaker
 config = Config('.env')
 
 DATABASE_URL = "mysql+pymysql://root:Ad147852@127.0.0.1:53000/checkup_ogi?charset=utf8mb4" # config('CU_DATABASE_URL', cast=str, default='')
-
-database = Database(DATABASE_URL)
-metadata = MetaData()
+# metadata = MetaData()
 engine = create_engine(DATABASE_URL)
-
-#add
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
