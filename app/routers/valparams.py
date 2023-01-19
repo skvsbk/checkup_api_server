97552b4dc0e76e7134bc834b883abe568780b662
skvsbk <skv.sbk@mail.ru>
2023-01-19 16:42:51 +0300

    rename

--- app/routers/valparams.py
+++ app/routers/valparams.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.schemas.valparam import Param
+from app.schemas.valparam import ValParam
 
 router = APIRouter()
 
-@router.get('/params', response_model=Param)
+@router.get('/params', response_model=ValParam)
 async def get_param():
     return {'param': 'param_1'}
