d0e3cd52d1ccc95b99e24e38385d353ee42f4b46
skvsbk <skv.sbk@mail.ru>
2023-01-19 10:00:10 +0300

    It is not need

--- app/models/database.py
+++ app/models/database.py
@@ -2,6 +2,10 @@ from databases import Database
 from sqlalchemy import create_engine, MetaData
 from starlette.config import Config
 
+#add
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 config = Config('.env')
 
@@ -10,3 +14,14 @@ DATABASE_URL = "mysql+pymysql://root:Ad147852@127.0.0.1:53000/checkup_ogi?charse
 database = Database(DATABASE_URL)
 metadata = MetaData()
 engine = create_engine(DATABASE_URL)
+
+#add
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/utils/base.py
+++ app/utils/base.py
@@ -1,6 +1,1 @@
-from databases import Database
 
-
-class BaseUtil:
-    def __init__(self, database: Database):
-        self.database = database
