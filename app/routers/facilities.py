889607e58badbcb634f0819a856e16f3addeed03
skvsbk <skv.sbk@mail.ru>
2023-01-19 18:34:10 +0300

    Add functionality

--- app/routers/facilities.py
+++ app/routers/facilities.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..models.database import get_db
+from app.schemas.facility import FacilityOut, FacilityCreate
+from ..utils import facilities_crud
 
 router = APIRouter()
 
-@router.get('/')
-async def get_facility():
-    return {'facilitiy': 'facility'}
+@router.get('/', response_model=list[FacilityOut])
+async def get_facility(db: Session = Depends(get_db)):
+    return facilities_crud.get_all(db=db)
+
+@router.post('/', response_model=FacilityOut)
+def create( value: FacilityCreate, db: Session = Depends(get_db)):
+    return facilities_crud.create(db=db, facility=value)
--- app/utils/facilities_crud.py
+++ app/utils/facilities_crud.py
@@ -1,16 +1,25 @@
-from base import BaseUtil
-from app.schemas.facility import Facility
+from sqlalchemy.orm import Session
+from app.schemas.facility import FacilityCreate
+from app.models import facilities
+from .base import create_base
 
 
-class FacilityUtils(BaseUtil):
-    async def get_all(self) -> Facility:
-        return
+def get_all(db: Session):
+    return db.query(facilities.FacilitiesDB).offset(0).limit(100).all()
 
-    async def create(self) -> Facility:
-        return
 
-    async def update(self) -> Facility:
-        return
+def create(db: Session, facility: FacilityCreate):
+    db_facility = facilities.FacilitiesDB(name=facility.name)
+    # db.add(db_facility)
+    # db.commit()
+    # db.refresh()
+    create_base(db, db_facility)
+    return db_facility
 
-    async def delete(self):
-        return
+
+def update(db: Session):
+    return
+
+
+def delete(db: Session):
+    return
