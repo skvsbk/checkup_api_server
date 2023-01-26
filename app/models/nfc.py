17faa11913188a605b562fc672150cbb288ccb55
skvsbk <skv.sbk@mail.ru>
2023-01-26 11:48:19 +0300

    refactoring

--- app/models/nfc.py
+++ app/models/nfc.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from ..models.database import Base
-from ..models.plants import PlantsDB
+from app.models.database import Base
+from app.models.plants import PlantsDB
 
 
 class NfcTagDB(Base):
-    __tablename__ = 'app.'
+    __tablename__ = 'nfc_tag'
     nfc_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     nfc_serial = Column(String(14))
     plant_id = Column(Integer, ForeignKey('plants.plant_id'), index=True)
 
-    plant = relationship('PlantsDB', backref='app.')
+    plant = relationship('PlantsDB', backref='nfc_tag')
--- app/models/users.py
+++ app/models/users.py
@@ -7,7 +7,7 @@ from app.models.roles import RoleDB
 class UserDB(Base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    login = Column(String(10))
+    login = Column(String(10), unique=True)
     password = Column(String(256))
     role_id = Column(Integer, ForeignKey('user_roles.role_id'), index=True)
     name = Column(String(50))
