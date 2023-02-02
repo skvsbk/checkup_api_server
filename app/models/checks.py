19cd73e75c632ca55f6a0702f4f2993c18cd5937
skvsbk <skv.sbk@mail.ru>
2023-02-02 17:57:36 +0300

    Modified model CheckDB

--- app/models/checks.py
+++ app/models/checks.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from app.models.database import Base
 from app.models.checkups import CheckupsDB
@@ -7,9 +7,9 @@ from app.models.nfc import NfcTagDB
 class ChecksDB(Base):
     __tablename__ = 'checks'
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    note = Column(String(256))
     checkup_id = Column(Integer, ForeignKey('checkups.id'), index=True)
     nfc_id = Column(Integer, ForeignKey('nfc_tag.id'), index=True)
+    t_check = Column(DateTime)
 
     checkups = relationship('CheckupsDB', backref='checks')
     nfctag = relationship('NfcTagDB', backref='checks')
