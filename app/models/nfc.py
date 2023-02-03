f65509b217df82b48f2b58780583b2123df58e7c
skvsbk <skv.sbk@mail.ru>
2023-02-03 08:54:20 +0300

    Add active field to NfcTagDB

--- app/models/nfc.py
+++ app/models/nfc.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from app.models.database import Base
 from app.models.plants import PlantsDB
@@ -9,5 +9,6 @@ class NfcTagDB(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     nfc_serial = Column(String(14))
     plant_id = Column(Integer, ForeignKey('plants.id'), index=True)
+    active = Column(Boolean)
 
     plant = relationship('PlantsDB', backref='nfc_tag')
